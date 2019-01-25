9e53049f2af5b7476764eef4a94b6a8822bac804
220OHM <47011415+220OHM@users.noreply.github.com>
2019-01-25 12:10:59 +0100

    the whole code
    the whole arduino code

---
+++ cat_laser.ino
@@ -0,0 +1,22 @@
+#include <Servo.h>
+Servo servop;
+Servo servo;  
+int val;    
+int val2;
+
+void setup() {
+  servop.attach(9);
+  servo.attach(10); 
+}
+
+void loop() {
+  val = analogRead(A0);            
+  val = map(val, 0, 1010, 0, 180);     
+  servop.write(val);                  
+  delay(15);
+
+  val2 = analogRead(1);            
+  val2 = map(val2, 0, 1010, 0, 180);     
+  servo.write(val2);                  
+  delay(15); 
+}
